61231267414f55ef7de138466bbfb74c44c70616
ShineLinn <142075907+ShineLinn@users.noreply.github.com>
2023-08-12 12:27:53 -0700

    Update twrp_selene.mk

--- twrp_selene.mk
+++ twrp_selene.mk
@@ -10,7 +10,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# $(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from selene device
 $(call inherit-product, device/xiaomi/selene/device.mk)
